a06e82498b3e9d4b2282d62db7f881158f39c274
Eddie Saunders <saunders.eddie@outlook.com>
2020-05-02 18:38:01 -0400

    Building out user selection options

--- employeeManager.js
+++ employeeManager.js
@@ -7,6 +7,7 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 
+// Build our connection string and connect to the database
 const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -15,7 +16,40 @@ const connection = mysql.createConnection({
     database: "employee_db"
 });
 
-connection.connect (function(error) {
+connection.connect(function (error) {
     if (error) throw error;
     console.log("connected as id " + connection.threadId);
+    start();
 });
+
+function displayLoginLogo() {
+    console.log("Connecting to Employee Database");
+    return;
+}
+
+function start() {
+    displayLoginLogo();
+    inquirer
+        .prompt(
+            {
+                name: "userSelection",
+                type: "list",
+                message: "What would you like to do today?",
+                choices: ["View All Employees", "View All Employees by Department", "View All Employees by Manager", "Add Employee", "Whack Employee", "Update Employee Role", "Update Employee Manager", "Leave This Application"]
+            })
+        .then(function (answer) {
+            console.log("Your selection was: ", answer);
+            switch (answer) {
+                case ("View All Employees"):
+                    console.log("Viewing all employees");
+                    break;
+                case ("View All Employees By Department"):
+                    console.log("View all by Dept");
+                    break;
+                default:
+                    console.log("An Error Occurred");
+                    console.log("You selected ", answer);
+                    connection.end();
+            };
+        });
+}
