b33105748e88a4a51535a5735bf734c7b2c20b1c
Eddie Saunders <saunders.eddie@outlook.com>
2020-05-03 04:36:45 -0400

    Added ASCII art

--- employeeManager.js
+++ employeeManager.js
@@ -28,7 +28,7 @@ function displayLoginLogo() {
 }
 
 function start() {
-    displayLoginLogo();
+    displayLogo();
     getUserInput();
 }
 
@@ -116,4 +116,29 @@ function updateEmployeeRole() {
 function updateEmployeeManager() {
     console.log ("Update Employee Manager");
     getUserInput();
+}
+
+
+function displayLogo() {
+    console.log('8888888888                        888                                     ');
+    console.log('888                               888                                     ');
+    console.log('888                               888                                     ');
+    console.log('8888888    88888b.d88b.  88888b.  888  .d88b.  888  888  .d88b.   .d88b.  ');
+    console.log('888        888 "888 "88b 888 "88b 888 d88""88b 888  888 d8P  Y8b d8P  Y8b ');
+    console.log('888        888  888  888 888  888 888 888  888 888  888 88888888 88888888 ');
+    console.log('888        888  888  888 888 d88P 888 Y88..88P Y88b 888 Y8b.     Y8b.     ');
+    console.log('8888888888 888  888  888 88888P"  888  "Y88P"   "Y88888  "Y8888   "Y8888  ');
+    console.log('                         888                        888                   ');
+    console.log('                         888                   Y8b d88P                   ');
+    console.log('                         888                    "Y88P"                    ');
+    console.log('8888888b.           888             888                                   ');
+    console.log('888  "Y88b          888             888                                   ');
+    console.log('888    888          888             888                                   ');
+    console.log('888    888  8888b.  888888  8888b.  88888b.   8888b.  .d8888b   .d88b.    ');
+    console.log('888    888     "88b 888        "88b 888 "88b     "88b 88K      d8P  Y8b   ');
+    console.log('888    888 .d888888 888    .d888888 888  888 .d888888 "Y8888b. 88888888   ');
+    console.log('888  .d88P 888  888 Y88b.  888  888 888 d88P 888  888      X88 Y8b.       ');
+    console.log('8888888P"  "Y888888  "Y888 "Y888888 88888P"  "Y888888  88888P"  "Y8888    ');
+                                                                              
+
 }
