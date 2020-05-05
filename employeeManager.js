f4a7a17e10d9d71240b9d2bc96d098f1b56b716d
Eddie Saunders <saunders.eddie@outlook.com>
2020-05-05 05:52:27 -0400

    Removed PW code

--- employeeManager.js
+++ employeeManager.js
@@ -15,7 +15,7 @@ const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "WestHam666!!", // naughty naughty code
+    password: "", // naughty naughty code
     database: "employee_db"
 });
 
